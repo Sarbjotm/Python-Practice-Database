1a8979c8c8d63bf678df7293610ef59d6641104e
Sarbjot <sarbjotmann98@gmail.com>
2021-01-05 01:22:34 -0800

    SQL insert/create/lookup functionality added

--- main.py
+++ main.py
@@ -1,26 +1,39 @@
 import sqlite3
 
 def add(name,money):
-    pass
+    c.execute(f"INSERT INTO persons VALUES ('{name}','{money}')")
 
 def update_money(name,updatedAge):
     pass
+
+def lookup(name):
+    c.execute(f"SELECT * FROM persons WHERE firstname='{name}'")
+    print(c.fetchone())
 
 def remove(name):
     pass
 
 def create():
-    pass
+    c.execute("""
+    CREATE TABLE IF NOT EXISTS persons(
+        firstname text,
+        money real
+    ) 
+    """)
 
+
+conn = sqlite3.connect('students.db')
+c = conn.cursor()
 create()
+conn.commit()
 print("Welcome to the database simulator")
 while(True):
-    user = input("What would you like to do: add an entry (add), update money (update) or remove an entry (remove):  ")
+    user = input("What would you like to do: add an entry (add), update money (update), lookup info (lookup) or remove an entry (remove) \n Press anything else to exit:  ")
     user = user.lower()
     if user == "add":
         name = input("What is the name of the user? ")
         dollars = input(f"How much money does {name} have? ")
-        add(user,dollars)
+        add(name,dollars)
     elif user == "update":
         name = input("Whose money amount would you like to change: ?")
         name = name.lower()
@@ -30,6 +43,12 @@ while(True):
         name = input("What is the name of the user that you want to remove? ")
         name = name.lower()
         remove(name)
+    elif user == "lookup":
+        name = input("What is the name of the user that you want to lookup? ")
+        lookup(name)
+    else:
+        break
 
     
-
+conn.commit()
+conn.close()
